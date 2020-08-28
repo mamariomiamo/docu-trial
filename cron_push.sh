9b3551d11dda0bcd8870d990f0812d16bb152d05
tsluser <tsluser@tslserver>
2020-08-28 22:54:48 +0800

    incremental backup

--- cron_push.sh
+++ cron_push.sh
@@ -3,7 +3,7 @@
 cd /home/tsluser/docusaurus_html/tl-tech-details/
 echo "Start git push now"
 
-git add .
+git add -A
 git commit -m "incremental backup" && git push
 
 echo "End git push"
--- docs/linux/getting-started/basics-usb-devices.md
+++
@@ -1,8 +0,0 @@
----
-hide_title: true
-sidebar_label: Basics - USB Devices
----
-
-`lsusb`
-
-`lsusb -t`
